b1f643347c1c222413a63dfb1da90478a7f92309
sai kumar <sai524470@gmail.com>
2022-11-13 06:45:33 +0530

    ui changes

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -38,8 +38,8 @@ const ProductScreen = () => {
             Go back
           </Link>
           <div className="mt-2">
-            <div className="grid gap-x-10 grid-cols-3">
-              <div className="mt-3 ">
+            <div className="grid gap-x-10 md:grid-cols-3">
+              <div className="mt-3 sm:w-2/4 md:w-full">
                 <img
                   src={product.image}
                   alt={product.name}
@@ -47,9 +47,9 @@ const ProductScreen = () => {
                 />
               </div>
 
-              <div className="mt-3 col-span-2">
+              <div className="mt-3 md:col-span-2">
                 <Heading title={product.name} />
-                <div className="grid grid-cols-2 gap-x-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4">
                   <div className="">
                     <ul className="">
                       <li className="p-3 mt-5 border-b-2">
@@ -88,7 +88,7 @@ const ProductScreen = () => {
                   </div>
                   <div>
                     <ul>
-                      <li className="rounded-md p-3 mt-5 border-b-2">
+                      <li className="p-3 mt-5 border-b-2">
                         <label class="text-sm mb-2 block text-gray-500">
                           Price
                         </label>
@@ -96,7 +96,7 @@ const ProductScreen = () => {
                           ${product.price}
                         </h3>
                       </li>
-                      <li className="rounded-md p-3 mt-1 border-b-2">
+                      <li className=" p-3 mt-1 md:border-b-2 mb-10 md:mb-0">
                         <label class="text-sm mb-2 block text-gray-500">
                           Stock
                         </label>
@@ -110,8 +110,8 @@ const ProductScreen = () => {
                           </h3>
                         )}
                       </li>
-                      <li className="rounded-md p-3 mt-1">
-                        <button className="bg-slate-900 w-full h-12 uppercase font-semibold text-white mt-1 rounded-lg">
+                      <li className="p-3 mt-1">
+                        <button className="bg-slate-900 w-full h-12 uppercase font-semibold text-white mt-1 md:rounded-lg fixed bottom-0 left-0 md:relative">
                           Add to cart
                         </button>
                       </li>
